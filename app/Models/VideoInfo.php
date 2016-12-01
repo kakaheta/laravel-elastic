f2ecd1e91b6d90a3c4c9cafe4cd95f335e8f13ce
zhangyimeng <zhangyimeng@letv.com>
2016-12-01 16:09:57 +0800

    commit

--- app/Models/VideoInfo.php
+++ app/Models/VideoInfo.php
@@ -8,4 +8,36 @@ use Sleimanx2\Plastic\Searchable;
 class VideoInfo extends Model
 {
     use Searchable;
+
+
+    // elasticsearch index name
+    public $documentIndex = 'Video';
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'video_info';
+
+    /**
+     * searchable property
+     *
+     * @var array
+     */
+    public $searchable = ['video_id', 'video_unique', 'media_id', 'user_id', 'video_name', 'video_desc', 'img', 'video_duration', 'video_file'];
+
+    /**
+     * If automatic sync data
+     */
+    public $syncDocument = false;
+
+
+    public function buildDocument()
+    {
+        return [
+            'video_id' => $this->video_id,
+            'video_unique' => $this->video_unique
+        ];
+    }
 }
